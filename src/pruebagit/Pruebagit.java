1f999d4ed2ee82f66fb81b4ceb0306fc5f3951b3
Borja <Borja@LAPTOP-6LQ7GUSO>
2021-11-07 13:52:49 +0100

    hola

---
+++ src/pruebagit/Pruebagit.java
@@ -0,0 +1,19 @@
+
+package pruebagit;
+
+/**
+ *
+ * @author Borja
+ */
+public class Pruebagit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        if(1 == 1)
+            System.out.println("lkbñk");
+    }
+    
+}
